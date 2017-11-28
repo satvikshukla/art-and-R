8e967116f282a000a7c82a6a134df8f0b3596687
satvik shukla <satvikshukla9@gmail.com>
2017-11-27 16:09:12 -0800

    app w/ rgb and w/ reg line

--- temp/app.R
+++ temp/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(highcharter)
+library(broom)
+library(dplyr)
 
 art.data <- read.csv("./../data1.csv", stringsAsFactors = FALSE)
 
@@ -18,7 +20,13 @@ ui <- fluidPage(
 		condition = "input.colspa == 'rgb'", selectInput(
 		  "rgbchoice", "Color choice", choices = list("Red" = "r", "Green" = "g", "Blue" = "b"), selected = 1
 		)
-	  )
+	  ),
+
+		conditionalPanel(
+			condition = "input.colspa == 'hsv'", selectInput(
+				"hsvchoice", "Color choice", choices = list("Hue" = "h", "Saturation" = "s", "Value" = "v"), selected = 1
+			)
+		)
 	),
 
 	mainPanel(
@@ -32,10 +40,22 @@ server <- function(input, output) {
 
 	output$distPlot <- renderHighchart({
 		str1 <<- paste0(input$colspa, ".", input$rgbchoice, ".avg")
+		
+		modlss <- loess(art.data[[str1]] ~ year, data = art.data)
+		fit <- augment(modlss) %>% arrange(year)
+		
+		#head(data)
+		#head(fit)
+		
 		hc <- highchart() %>% 
-				hc_add_series(art.data, "scatter", hcaes(x = year, y = art.data[[str1]])) %>% 
-				hc_add_theme(hc_theme_elementary())
-
+			hc_add_series(art.data, "scatter", hcaes(x = year, y = art.data[[str1]])) %>% 
+			hc_add_theme(hc_theme_elementary()) %>%
+			hc_add_series(fit, type = "spline", hcaes(x = year, y = .fitted), name = "Fit",
+						  id = "fit")# %>% 
+			#hc_add_series(fit, type = "arearange",
+			#			  hcaes(x = year, low = .fitted - .se.fit*2, high = .fitted + .se.fit*2),
+			#			  linkedTo = "fit")
+		
 		hc
 
 	})
